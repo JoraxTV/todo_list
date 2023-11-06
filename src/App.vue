a51b9a7ad0ef2b1d08a8798d1cf72f91ace69415
JoraxTV <jojodu34dadada@gmail.com>
2023-11-06 12:16:17 +0100

    Exercice 20

--- src/App.vue
+++ src/App.vue
@@ -1,98 +1,80 @@
 <script setup lang="ts">
- import {ref, computed} from 'vue';
-  let id = 0;
-  const taches = ref([{id:id++, description:"Apprendre Vue", faite:false},
-    {id:id++, description:"Finir la SAÉ", faite:false},
-    {id:id++, description:"Réviser pour l'interro", faite:false}]);
-  
-  let description = "";
-  function ajouterTache(description:string){
-    if (description == "") return;
-    taches.value.push({id:id++, description:description, faite:false});
+  import { ref, computed } from 'vue';
+  import type { Ref } from 'vue';
+
+
+  interface Tache {
+    id: number;
+    description: string;
+    faite: boolean;
   }
 
-  function retirerTache(id:number){
+  
+  let id = 0;
+  const taches:Ref<Tache[]> = ref([{ id: id++, description: "Apprendre Vue", faite: false },
+  { id: id++, description: "Finir la SAÉ", faite: false },
+  { id: id++, description: "Réviser pour l'interro", faite: false }]);
+
+
+  let description = "";
+  function ajouterTache(description: string) {
+    if (description == "") return;
+    taches.value.push({ id: id++, description: description, faite: false });
+  }
+
+
+  function retirerTache(id: number) {
     taches.value = taches.value.filter(tache => tache.id != id);
   }
 
-  const cacheFaits = ref(false);
 
+  const cacheFaits = ref(false);
   const tachesFiltrees = computed(() => {
     return cacheFaits.value ? taches.value.filter(tache => !tache.faite) : taches.value;
   });
 </script>
 
 <template>
+  <input type="text" v-model.trim="description" placeholder="Ajouter une tâche" /> <button
+    @click="ajouterTache(description)">Ajouter</button>
+
+  <div id="wrapper">
+    <ul>
+      <li v-for="tache in tachesFiltrees" :key="tache.id">
+        <input type="checkbox" v-model="tache.faite" />
+        <span class="description" :class="{fait:tache.faite, pasfait:!tache.faite}">{{tache.description}}</span>
+        <button @click="retirerTache(tache.id)">Retirer</button>
+      </li>
+    </ul>
+  </div>
   <button @click="cacheFaits = !cacheFaits">
     {{ cacheFaits ? 'Tout montrer' : 'Cacher les tâches terminées' }}
-</button>
-  <input type="text" v-model.trim="description" placeholder="Ajouter une tâche"/>
-  <button @click="ajouterTache(description)">Ajouter</button>
-  <div id="wrapper">
-      <ul>
-        <li v-for="tache in tachesFiltrees" :key="tache.id">
-          <input type="checkbox" v-model="tache.faite"/>
-          <span class="description" :class="{fait:tache.faite, pasfait:!tache.faite}">{{tache.description}}</span>
-          <button @click="retirerTache(tache.id)">Retirer</button>
-        </li>
-      </ul>
-    </div>
+  </button>
 </template>
 
 <style scoped>
-body{
-  font-family: sans-serif;
-  font-size: 1.2rem;
-  line-height: 1.5;
-  color: #333;
-  margin: 0;
-  padding: 0;
-}
-
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  #wrapper {
+    border-radius: 5px;
+    border: solid grey 2px;
+    padding: 10px;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
+  ul,
+  span {
+    padding: 10px;
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  li {
+    list-style: none;
+    padding: 2px 0px;
   }
-}
-#wrapper{
-   border-radius: 5px;
-   border:solid black 2px;
-   padding: 10px;
- }
- ul,span{
-   padding:10px;
- }
- li{
-   list-style: none;
-   padding: 2px 0px;
- }
- 
- .fait{
-   text-decoration: line-through;
-   color: grey;
- }
- .pasfait{
-   color: white;
- }
+
+  .fait {
+    text-decoration: line-through;
+    color: grey;
+  }
+
+  .pasfait {
+    color: white;
+  }
 </style>
