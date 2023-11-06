14768ebb3266fbf830f7e48a20716e5bddef5fcc
JoraxTV <jojodu34dadada@gmail.com>
2023-11-06 11:01:54 +0100

    Exercice 6

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,15 @@ function incremente() {
   compteur.value++;
   console.log(compteur.value);
 }
+function onInput(event : any) {
+  compteur.value = event.target.value;
+}
 </script>
 
 <template>
   <h1><a v-bind:href="url">Site de Vue</a></h1>
   <button v-on:click="incremente">{{ compteur }}</button>
+  <input type="number" v-on:input="onInput" v-bind="compteur" />
 </template>
 
 <style scoped>
